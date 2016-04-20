24d3bc2e19fbec721afa142b787acb13c4f58c46
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-20 15:24:19 -0600

    Fix functionality to refresh user token
    Migration to change token expiry date to datetime object instead of string
    to make comparison with current time easier
    Fixed error in user from auth hash, was invoking bad hash key for auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :validate_user_auth_token
+  before_action :current_user
+  before_action :validate_auth_token
 
   def current_user
     @current_user ||= User.find_by(uid: session[:uid]) if session[:uid]
   end
 
-  def validate_user_auth_token
-    if @current_user
-      @current_user.validate_auth_token
-    end
+  def validate_auth_token
+    @current_user.validate_auth_token if @current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:uid] = user.uid
+      user.validate_auth_token
     else
       flash[:errors] = "There was a problem signing in."
     end
