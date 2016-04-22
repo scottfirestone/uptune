7caf39e3d1294d64d3812c4a7b08bbb21615fc2f
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-22 14:22:52 -0600

    Add sad path for playlist show that doesn't exist
    Renders a 404 if the playlist code has not been created

--- spec/features/guest_can_join_created_playlist_spec.rb
+++ spec/features/guest_can_join_created_playlist_spec.rb
@@ -2,15 +2,22 @@ require "rails_helper"
 
 RSpec.feature "Guest can join created playlist" do
   scenario "they see the playlist page for that playlist" do
-      playlist = create(:playlist)
-      playlist.update(code: "xyz")
+    playlist = create(:playlist)
+    playlist.update(code: "xyz")
 
-      visit "/playlist/xyz"
+    visit "/playlist/xyz"
 
-      expect(current_path).to eq(playlist_path(playlist.code))
-      expect(page).to have_content("Search Spotify")
-      within(".playlist-code") do
-        expect(page).to have_content("Your Playlist Code is: #{playlist.code}")
+    expect(current_path).to eq(playlist_path(playlist.code))
+    expect(page).to have_content("Search Spotify")
+    within(".playlist-code") do
+      expect(page).to have_content("Your Playlist Code is: #{playlist.code}")
     end
+  end
+
+  scenario "they see a 404 if trying a playlist not created" do
+    visit "/playlist/foobar"
+
+    expect(page).to have_content("404")
+    expect(page).to_not have_content("Your Playlist Code is: foobar")
   end
 end
