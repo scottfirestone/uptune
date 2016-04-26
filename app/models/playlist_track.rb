c2a61e6819a088fc9353bc6c38f6dcbd1c84a95e
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-25 21:00:29 -0600

    Default scope playlist tracks by vote count
    Playlist tracks are now ordered by vote count, and will
    rearrange on playlist show page accordingly

--- app/models/playlist_track.rb
+++ app/models/playlist_track.rb
@@ -1,10 +1,15 @@
 class PlaylistTrack < ActiveRecord::Base
   belongs_to :playlist
   belongs_to :track
-
   has_many :votes
 
   validates_uniqueness_of :track_id, { scope: :playlist_id }
+
+  default_scope {
+    joins(:votes)
+    .group(:id)
+    .order("votes.count DESC")
+  }
 
   def vote_count
     votes.count
